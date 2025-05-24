41ca8ead0b0a61e2624cb486fe5b05183eae48c4
felo <lfalvaz@gmail.com>
2025-05-24 10:37:19 -0500

    style updates

--- lf-team-profiles.php
+++ lf-team-profiles.php
@@ -227,13 +227,7 @@ class LF_Team_Profiles {
             $output .= '<img src="' . esc_url($photo_url) . '" alt="' . esc_attr($name) . '" class="lf-team-photo">';
             $output .= '</div>';
             $output .= '<div class="lf-team-name-wrapper">';
-            $output .= '<h3 class="lf-team-name">' . esc_html($name);
-            if ($linkedin) {
-                $output .= ' <a href="' . esc_url($linkedin) . '" target="_blank" rel="noopener noreferrer" class="lf-team-linkedin-icon" onclick="event.stopPropagation();">';
-                $output .= '<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>';
-                $output .= '</a>';
-            }
-            $output .= '</h3>';
+            $output .= '<h3 class="lf-team-name">' . esc_html($name) . '</h3>';
             if ($job_title) {
                 $output .= '<p class="lf-team-job-title">' . esc_html($job_title) . '</p>';
             }
